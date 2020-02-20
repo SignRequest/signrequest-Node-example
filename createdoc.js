da2cef93038cfb29252874f3f4e82ad32de2ed08
Lucas Pascholatti <lucaspascholatti@Lucass-MacBook-Air.local>
2020-02-20 12:56:41 +0100

    Adding quicklycreate doc and sign request

--- createdoc.js
+++ createdoc.js
@@ -5,10 +5,10 @@ const baseUrl = "https://yourteam.signrequest.com/api/v1";
 // 1) Get Documents:
 
 const getDocs = () => {
-    request(`${baseUrl}/documents/`)
-    .set("Authorization", "Token your_token_here")
+  request(`${baseUrl}/documents/`)
+    .set("Authorization", "Token YOUR_TOKEN_HERE")
     .then(response => {
-        console.log("Response: ", response.body)
+      console.log("Response: ", response.body);
     })
     .catch(console.error);
 };
@@ -18,19 +18,27 @@ getDocs();
 // 2) Create a New Document:
 
 const data = {
-    file_from_url: "your_file_pdf"
+    file_from_url: "your_url_here"
+//   template:
+//     "https://yourteam.signrequest.com/api/v1/templates/uuid/",
+//   prefill_tags: [
+//       {external_id: "contact01_name", text: "contact01"},
+//       {external_id: "contact02_name", text: "contact02"},
+//       {external_id: "date_prefill01", date_value: "YYYY-MM-DD"},
+//       {external_id: "date_prefill02", date_value: "YYYY-MM-DD"}
+//   ],
+//   auto_delete_days: 1
 };
 
 const createDoc = () => {
-    request
+  request
     .post(`${baseUrl}/documents/`)
-    .set("Authorization", "Token your_team_token")
+    .set("Authorization", "Token YOUR_TOKEN_HERE")
     .send(data)
     .then(response => {
-        console.log("Response: ", response.body)
+      console.log("Response: ", response.body);
     })
     .catch(console.error);
 };
 
 createDoc();
-
--- quicklycreate.js
+++ quicklycreate.js
@@ -2,10 +2,8 @@ const request = require("superagent");
 
 const baseUrl = "https://yourteam.signrequest.com/api/v1";
 
-// 3) Create a Sign Request:
-
 const data = {
-    document: "https://yourteam.signrequest.com/api/v1/documents/uuid/",
+    file_from_url: "your_url_here",
     signers: [
         {
             email: 'name+1@provider.com'
@@ -18,12 +16,13 @@ const data = {
     message: "Please sign this document",
     needs_to_sign: true,
     who: "o",
-    subject: "SignTest - YourTeam API"
+    subject: "SignTest - YourTeam API",
+    auto_delete_days: 1
 };
 
 const createSignRequest = () => {
     request
-    .post(`${baseUrl}/signrequests/`)
+    .post(`${baseUrl}/signrequest-quick-create/`)
     .set("Authorization", "Token YOUR_TOKEN_HERE")
     .send(data)
     .then(response => {
