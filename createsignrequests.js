ff0291e6d971f6c3aacb1446bd4aaf5c7c312fe5
Lucas Pascholatti <lucaspascholatti@Lucass-MacBook-Air.local>
2020-03-20 16:48:58 +0100

    Added required_attachments

--- createsignrequests.js
+++ createsignrequests.js
@@ -5,40 +5,47 @@ const baseUrl = "https://yourteam.signrequest.com/api/v1";
 // 3) Create a Sign Request:
 
 const data = {
-    document: "https://yourteam.signrequest.com/api/v1/documents/uuid/",
-    signers: [
-        {
-            email: 'name+1@provider.com',
-            //order: 1,
-            //embeded_url_id: "id01",
-            //redirect_url: "add_url", (for an embed_url)
-            //password: "add_password", (optional)
-            //after_document: "add_previous_document_here" (to chain multiple documents)
-        },
-        {
-            email: 'name+2@provider.com',
-            //order: 2,
-            //embeded_url_id: "id02",
-            //redirect_url: "add_url", (for an embed_url)
-            //password: "add_password", (optional)
-            //after_document: "add_previous_document_here" (to chain multiple documents)
-        }
-    ],
-    from_email: "name+3@provider.com",
-    message: "Please sign this document",
-    needs_to_sign: true,
-    who: "o",
-    //it can also be "mo" (me and others), "m" (only me)
-    subject: "SignTest - YourTeam API"
+  document: "https://yourteam.signrequest.com/api/v1/documents/uuid/",
+  signers: [
+    {
+      email: "name+1@provider.com"
+      //order: 1,
+      //embeded_url_id: "id01",
+      //redirect_url: "add_url", (for an embed_url)
+      //password: "add_password", (optional)
+      //after_document: "add_previous_document_here" (to chain multiple documents)
+    },
+    {
+      email: "name+2@provider.com"
+      //order: 2,
+      //embeded_url_id: "id02",
+      //redirect_url: "add_url", (for an embed_url)
+      //password: "add_password", (optional)
+      //after_document: "add_previous_document_here" (to chain multiple documents)
+    }
+  ],
+  from_email: "name+3@provider.com",
+  message: "Please sign this document",
+  needs_to_sign: true,
+  who: "o",
+  //it can also be "mo" (me and others), "m" (only me)
+  subject: "SignTest - YourTeam API"
+  //required_attachments:
+  //[
+  // {name: "Passport"},
+  // {name: "Driver License"},
+  // {name: "ID"}
+  //],
+  //
 };
 
 const createSignRequest = () => {
-    request
+  request
     .post(`${baseUrl}/signrequests/`)
     .set("Authorization", "Token YOUR_TOKEN_HERE")
     .send(data)
     .then(response => {
-        console.log("Response: ", response.body)
+      console.log("Response: ", response.body);
     })
     .catch(console.error);
 };
